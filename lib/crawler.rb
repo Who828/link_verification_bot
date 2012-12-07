83b0e7959850bd5ea3646acb51871decf19cb828
Smit Shah <who828@gmail.com>
2012-12-07 11:00:19 +0530

    Added method to resolve links

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -1,7 +1,21 @@
 require 'nokogiri'
+require 'open-uri'
 
 class Crawler
-  def parse(filename)
-    Nokogiri::HTML(open(filename))
+  attr_reader :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  def parse
+    doc = Nokogiri::HTML(open(url))
+    doc.css('a').map { |link| absolute_url(link['href']) }
+  end
+
+  private
+
+  def absolute_url(link)
+    URI.join(url, link).to_s
   end
 end
--- spec/crawler_spec.rb
+++ spec/crawler_spec.rb
@@ -1,9 +1,21 @@
 require 'crawler'
+require 'vcr_setup'
 
 describe Crawler do
   it "gives you a list of URL on a page" do
-    crawler = Crawler.new
-    result = crawler.parse('fixtures/test.html')
-    result.content =~ /google.com/
+    VCR.use_cassette('blog-42') do
+      crawler = Crawler.new('http://blog-42-staging.herokuapp.com/')
+      result = crawler.parse
+      result.first.should =~ /\/auth\/google/
+      result.length.should == 1
+    end
+  end
+
+  it "gives you an absolute url for the page" do
+    VCR.use_cassette('blog-42') do
+      crawler = Crawler.new('http://blog-42-staging.herokuapp.com/')
+      result = crawler.parse
+      result.should include "http://blog-42-staging.herokuapp.com/auth/google"
+    end
   end
 end
